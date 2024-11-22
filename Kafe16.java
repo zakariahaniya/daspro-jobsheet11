87a54c4f771c2b1beaaa35b4c78881d02ac527fc
zakariahaniya <zakalvha@gmail.com>
2024-11-22 13:41:30 +0700

    daspro jobsheet 11

---
+++ Kafe16.java
@@ -0,0 +1,17 @@
+public class Kafe16 {
+
+    public static void menu() {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("===========================");
+        System.out.println("Silahkan pilih menu yang Anda inginkan.");
+    }
+    public static void main(String[] args) {
+        menu();
+    }
+}
